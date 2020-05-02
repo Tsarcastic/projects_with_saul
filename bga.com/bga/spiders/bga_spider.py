6b7e2ee09a2ffc0fa269667d56ba1eb7e32f51c1
Brendan Davis <BrendanMD@gmail.com>
2020-05-01 19:37:13 -0700

    index page is now retrieving properly

--- bga.com/bga/spiders/bga_spider.py
+++ bga.com/bga/spiders/bga_spider.py
@@ -8,9 +8,9 @@ class BgaSpider(scrapy.Spider):
     name = 'bga_crawler'
     allowed_domains = ['boardgamearena.com']
     """start_urls = ['http://boardgamearena.com/gamelist?section=all']"""
-    start_urls = ['https://boardgamearena.com/gamepanel?game=raceforthegalaxy']
+    start_urls = ['https://boardgamearena.com/gamelist?section=all']
     # Create extractor for index page
-    index_page_etractor = selectorlib.Extractor.from_yaml_file(os.path.join(os.path.dirname(__file__),'../selectorlib_yml/index.yml'))
+    index_page_extractor = selectorlib.Extractor.from_yaml_file(os.path.join(os.path.dirname(__file__),'../selectorlib_yml/index.yml'))
     # Create extractor for detail page
     """detail_page_extractor = selectorlib.Extractor.from_yaml_file(os.path.join(os.path.dirname(__file__),'../selectorlib_yml/board_game_detail.yml'))"""
 
@@ -18,8 +18,9 @@ class BgaSpider(scrapy.Spider):
 
     def parse(self, response):
         # Extract data using Extractor
-        data = self.detail_page_extractor.extract(response.text)
+        data = self.index_page_extractor.extract(response.text)
+        #data = self.detail_page_extractor.extract(response.text)
         yield data
 
-div#gamecategory_wrap_all.pagesection div.gamelist_itemrow_inner
-div#gamelist_itemrow_inner_all div.
+"""div#gamecategory_wrap_all.pagesection div.gamelist_itemrow_inner
+div#gamelist_itemrow_inner_all div."""
