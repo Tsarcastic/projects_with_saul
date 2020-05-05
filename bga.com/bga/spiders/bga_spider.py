38df885a1ae61e7eccea318a3cf786c1cb6da682
Brendan Davis <BrendanMD@gmail.com>
2020-05-04 18:11:46 -0700

    working with the test list

--- bga.com/bga/spiders/bga_spider.py
+++ bga.com/bga/spiders/bga_spider.py
@@ -3,20 +3,39 @@ import scrapy
 import os
 import selectorlib
 
-url_prefix = prefix = 'boardgamearena.com/gamepanel?game='
+opened_file = open(os.path.join(os.path.dirname(__file__), '../selectorlib_yml/test_tags.txt') , 'r')
+gametags = opened_file.read()
+opened_file.close()
+big_string = ''.join(gametags)
+game_list = big_string.split()
+print(game_list)
+prefix = 'http://boardgamearena.com/gamepanel?game='
+urls = []
+
+
+for x in game_list:
+    #print(x)
+    the_url = prefix + x
+    urls.append(the_url)
 
 
 class BgaSpider(scrapy.Spider):
     name = 'bga_crawler'
     allowed_domains = ['boardgamearena.com']
+    start_urls = urls
+
+
+
     """start_urls = ['http://boardgamearena.com/gamelist?section=all']"""
-    start_urls = ['https://boardgamearena.com/gamepanel?game=sechsnimmt']
+    """start_urls = ['https://boardgamearena.com/gamepanel?game=sechsnimmt']"""
 
     """# Create extractor for index page
     index_page_extractor = selectorlib.Extractor.from_yaml_file(os.path.join(os.path.dirname(__file__),'../selectorlib_yml/index.yml'))"""
 
     # Create extractor for detail page
     detail_page_extractor = selectorlib.Extractor.from_yaml_file(os.path.join(os.path.dirname(__file__),'../selectorlib_yml/board_game_detail.yml'))
+
+
 
 
 
